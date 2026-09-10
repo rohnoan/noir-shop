92d8a16957c38a464d093d0236df60f370c6338b
rohan <sharmarohan2507@gmail.com>
2026-09-10 11:40:11 +0530

    working build

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import ShopClient from "@/components/ShopClient";
 
 export default function ShopPage() {
-  return <ShopClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ShopClient />
+    </Suspense>
+  );
 }
